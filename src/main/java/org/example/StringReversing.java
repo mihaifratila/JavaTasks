f8c7e4310bab88e515ac72bddd1bc06802dc7ecc
mihaifratila <mfratila@griddynamics.com>
2023-01-23 15:36:40 -0800

    add the second string reverse method

--- src/main/java/org/example/StringReversing.java
+++ src/main/java/org/example/StringReversing.java
@@ -1,16 +1,27 @@
 package org.example;
 
 public class StringReversing {
-    public static String stringReverse1(String s1) {
+    public static String stringReverse1(String s) {
         String reversedString ="";
-        for (int i = s1.length() - 1; i >=0; i--) {
-            reversedString += s1.charAt(i);
+        for (int i = s.length() - 1; i >=0; i--) {
+            reversedString += s.charAt(i);
         }
         return reversedString;
     }
 
+    public static String stringReverse2(String s) {
+        char temp =' ';
+        char[] stringToCharArray = s.toCharArray();
+        for (int i = 0, j = stringToCharArray.length - 1; i < j; i++, j--) {
+            temp = stringToCharArray[j];
+            stringToCharArray[j] = stringToCharArray[i];
+            stringToCharArray[i] = temp;
+        }
+        return new String(stringToCharArray);
+    }
+
     public static void main(String[] args) {
         String stringToRevers = "I'm going to go to school today morning.";
-        System.out.println(stringReverse1(stringToRevers));
+        System.out.println(stringReverse2(stringToRevers));
     }
 }
