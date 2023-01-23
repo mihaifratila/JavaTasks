0e223f2691f1d4cd0caad99849c295b7aff8f1b2
mihaifratila <mfratila@griddynamics.com>
2023-01-23 10:22:03 -0800

    Add First method for string reversing

--- src/main/java/org/example/StringReversing.java
+++ src/main/java/org/example/StringReversing.java
@@ -1,5 +1,16 @@
 package org.example;
 
 public class StringReversing {
+    public static String stringReverse1(String s1) {
+        String reversedString ="";
+        for (int i = s1.length() - 1; i >=0; i--) {
+            reversedString += s1.charAt(i);
+        }
+        return reversedString;
+    }
 
+    public static void main(String[] args) {
+        String stringToRevers = "I'm going to go to school today morning.";
+        System.out.println(stringReverse1(stringToRevers));
+    }
 }
