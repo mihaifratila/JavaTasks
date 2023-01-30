b90fb94dd32cebcab033efb5c930455cd3f74269
mihaifratila <mfratila@griddynamics.com>
2023-01-30 10:28:37 +0200

    Add solved Problems for the Third Task from Java Module 2

--- src/main/java/org/example/Task2.java
+++ src/main/java/org/example/Task2.java
@@ -1,17 +1,18 @@
 package org.example;
 
+import java.util.LinkedList;
 import java.util.regex.*;
 
 public class Task2 {
     public static void main(String[] args) {
-        //Task 1: Find and return true or false if String contains orderUUID
+        //Part 1: Find and return true or false if String contains orderUUID
         String input1 = "app=edi_adapter_converter wingtipsTrace=8faeae6709355291 INFO  OrderCreateClient - action=EDIOrderSent originalFilename=Integration_test_Contract customerName=0005084863 orderUUID=d34149d8-88ab-4791-bb0a-46c96e034200 poNum=Test_TS5155079515 lineCount=3";
 
         boolean result = input1.matches(".*orderUUID.*");
 
         System.out.println(result);
 
-        // Task 2: Find and return orderUUID substring from string
+        // Part 2: Find and return orderUUID substring from string
         String input2 = "app=edi_adapter_converter wingtipsTrace=8faeae6709355291 INFO  OrderCreateClient - action=EDIOrderSent originalFilename=Integration_test_Contract customerName=0005084863 orderUUID=d34149d8-88ab-4791-bb0a-46c96e034200 poNum=Test_TS5155079515 lineCount=3";
 
         Pattern pattern = Pattern.compile("orderUUID=([\\S]+)");
@@ -22,7 +23,7 @@ public class Task2 {
             System.out.println(orderUUID);
         }
 
-        // Task 3: Find and return user email from string
+        // Part 3: Find and return user email from string
         String input3 = "test 2667843 (test_email@griddynamics.com) test 67483 some string";
 
         Pattern pattern2 = Pattern.compile("\\(([\\S]+@[\\S]+)\\)");
@@ -33,7 +34,7 @@ public class Task2 {
             System.out.println(email);
         }
 
-        // Task 4: Find and return how many orders were created from this string
+        // Part 4: Find and return how many orders were created from this string
         String input4 = "app=edi_adapter_splitter wingtipsTrace=225debfbe6e5fac7 poiFileName=Integration_test_Contract INFO  LogUtils - POI file name: [Integration_test_Contract], total number of orders successfully processed: [2]";
 
         Pattern pattern3 = Pattern.compile("[\\d]");
@@ -42,6 +43,24 @@ public class Task2 {
         if(matcher3.find()) {
             String orderNumber = matcher3.group();
             System.out.println(orderNumber);
+        }
+
+        // Part 5: Print to console all the words that start on 'de'.
+
+        String textForSearch = "When you first start to evaluate a cloud migration, the decisions to be made, stakeholders who need to be involved, it can sometimes feel overwhelming – and with good reason. A move to the cloud can take time, resources, and manpower, and while companies are nearly universally glad they made those investments, that doesn't cancel out the fact that they take strategic planning and up-front work."
+                + " What do you need to plan for? Who needs to be involved (and when)? What roles and responsibilities need to be divided up? The answers vary a bit depending on your company size, the scope of your migration, and whether you have the budget (or need) for a migration partner."
+                + " Below are the typical roles and responsibilities we see from successful migrations – and some tips to prepare your team for success. Because without the right team in place, missed deadlines, duplicative work, and post-migration workflow issues become much greater risks.";
+
+        String[] textAsArray = textForSearch.split(" ");
+        LinkedList<String> wordsList = new LinkedList<>();
+        for(String word: textAsArray) {
+            if(word.startsWith("de")) {
+                wordsList.add(word);
+            }
+        }
+
+        for (String s : wordsList) {
+            System.out.println(s);
         }
     }
 }
